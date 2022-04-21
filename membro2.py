83080cce7437744e1f8a5166d2e99d83a5771330
Lívia <livia@email.com>
2022-04-21 20:53:05 -0300

    membro2

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Lívia, tenho 19 anos, gosto de jogar vôlei e ir para academia. Já fiz um ano de ciências da computação na Unisul, mas não tenho muito conhecimento ainda sobre programação. Espero aprender muito com a disciplina!" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
