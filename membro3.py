2733c08569efadf369a50a421f9b6c3390cdbd76
eduardohjr <104038643+eduardohjr@users.noreply.github.com>
2022-04-23 16:33:49 -0300

    Update membro3.py

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Eduardo Ribeiro Heitor Junior, tenho 19 anos, gosto de jogos, conheço um pouco de python e espero melhores meus conhecimentos sobre programação orientada a objetos." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
